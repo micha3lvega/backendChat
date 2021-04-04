bd09fec7a75b5a64313b732586c42f047427839b
Michael Vega <ECM3572R@claro.com.co>
2021-04-04 03:13:16 -0500

    Agregar propiedades

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var initConfig = require('./initConfig');
 
-var indexRoute = require('./middleware/index');
-initConfig.app.use('/', indexRoute);
-
-const logger = initConfig.log4js.getLogger('server.js');
+var logger = initConfig.log4js;
 logger.level = process.env.LOG_LEVEL || 'ALL';
 
+const app = initConfig.app;
 const port = initConfig.port;
 
+var indexRoute = require('./middleware/index');
+app.use('/', indexRoute);
+
+const port = initConfig.port;
 initConfig.app.listen(port, () => logger.info('Example app listening on port %d!!!', port));
