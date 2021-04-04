1e7f35b7ebb3e6193e9831acd1d584dd4d3011fd
Michael Vega <ECM3572R@claro.com.co>
2021-04-04 03:44:19 -0500

    Cambiar mensaje al iniciar servidor

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const log4js = initConfig.log4js;
 var logger = log4js.getLogger('server.js');
 logger.level = process.env.LOG_LEVEL || 'ALL';
 
-app.listen(port, () => logger.info('Example app listening on port %d!!!', port));
+app.listen(port, () => logger.info('Aplicacion inicio con exito por el puerto: %d!!!', port));
