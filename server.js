29f5d841ac508958aa388a5af8c3ff9aeeedbec3
Michael Vega <ECM3572R@claro.com.co>
2021-04-04 00:59:01 -0500

    Cambiar funcion para usar el router para solo usar peticiones get

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const route = express.Router();
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => {
-    console.log('Example app listening on port %d!!!', port);
-});
+app.use(route);
+
+route.get("/", function(req,res) {
+    res.send("Hola mundo");
+})
+
+app.listen(port, () => console.log('Example app listening on port %d!!!', port));
+
