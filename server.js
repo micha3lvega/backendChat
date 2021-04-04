ae05e5857d71fdbec8b8c693887aa7046259f221
Michael Vega <ECM3572R@claro.com.co>
2021-04-04 01:18:59 -0500

    Agregar nuevo routing al archivo server.js

--- server.js
+++ server.js
@@ -4,23 +4,7 @@ const route = express.Router();
 const app = express();
 const port = 3000;
 
-app.use(route);
-
-route.get("/", function(req,res) {
-    res.send("Hola mundo -- protocolo get");
-});
-
-route.post("/", function(req,res) {
-    res.send("Hola mundo -- protocolo post");
-});
-
-route.put("/", function(req,res) {
-    res.send("Hola mundo -- protocolo put");
-});
-
-route.delete("/", function(req,res) {
-    res.send("Hola mundo -- protocolo delete");
-});
+var indexRoute = require('./middleware/index');
+app.use('/', indexRoute);
 
 app.listen(port, () => console.log('Example app listening on port %d!!!', port));
-
