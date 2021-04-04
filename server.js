293430bdf9342ddd2328d86e7176317f345e87d9
Michael Vega <ECM3572R@claro.com.co>
2021-04-04 02:45:58 -0500

    Agegar configuracion de log

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ require('dotenv').config({
     path: `./env/${NODE_ENV}.env`
 });
 
+const log4js = require('log4js');
+var logger = log4js.getLogger('server.js');
+logger.level = process.env.LOG_LEVEL || 'ALL';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 var indexRoute = require('./middleware/index');
 app.use('/', indexRoute);
 
-app.listen(port, () => console.log('Example app listening on port %d!!!', port));
+app.listen(port, () => logger.info('Example app listening on port %d!!!', port));
